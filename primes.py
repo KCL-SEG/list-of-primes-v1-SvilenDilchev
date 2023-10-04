70ef942e6a5f447e9911c0c44130aaa5d14ae260
Svilen Dilchev <svilen.dilchev@kcl.ac.uk>
2023-10-04 21:29:15 +0300

    Add a prime number generator

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes == 1:
+        return [2]
+
+    counter = 2
+    while len(list) < number_of_primes:
+
+        isPrime = True
+        for prime in list:
+            if counter % prime == 0:
+                isPrime = False
+
+        if isPrime:
+            list.append(counter)
+        counter += 1
+
     return list
+
